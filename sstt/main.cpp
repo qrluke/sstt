4d87a402388d02e81b61a21eec9971a10f2c1802
qrlk <hi@qrlk.me>
2020-06-02 17:44:01 +0300

    Fixed missing CheckKey("L");

--- sstt/main.cpp
+++ sstt/main.cpp
@@ -308,7 +308,7 @@ void MainThread()
 
 			initialized = true;
 
-			pSAMP->AddChatMessage(-1, "SSTT v02.06.2020 инициализирован. Держите клавишу, потом отпустите. Автор: qrlk.me");
+			pSAMP->AddChatMessage(-1, "SSTT v02.06.2020-2 инициализирован. Держите клавишу, потом отпустите. Автор: qrlk.me");
 			pSAMP->AddChatMessage(-1, "Github: https://github.com/qrlk/sstt");
 			pSAMP->AddChatMessage(-1, "Клавиши: R - говорить, P - крикнуть, N - рация, M - /me, L - мегафон, B - /b");
 		}
@@ -317,7 +317,9 @@ void MainThread()
 		CheckKey("P");
 		CheckKey("N");
 		CheckKey("B");
+		CheckKey("L");
 		CheckKey("M");
+
 	}
 }
 
